c03406b472086ae93910612cc891bcd09cedc06b
bakanano <17448534+bakanano@users.noreply.github.com>
2021-02-26 15:01:45 +1300

    Add files via upload

--- app.js
+++ app.js
@@ -1,85 +1,79 @@
-var signedIn = false;
-var loginUsername = document.getElementById("loginUsername");
-var loginPassword = document.getElementById("loginPassword");
-
 window.onload = () => {
   showHomeTab();
-  document.getElementById("userName").value = "";
-  document.getElementById("userMessage").value = "";
 };
 
 function showHomeTab() {
-  document.getElementById("home").style.display = "block";
-  document.getElementById("products").style.display = "none";
-  document.getElementById("location").style.display = "none";
-  document.getElementById("news").style.display = "none";
-  document.getElementById("guest").style.display = "none";
-  document.getElementById("register").style.display = "none";
-  document.getElementById("signin").style.display = "none";
+  document.querySelector(".home").style.display = "block";
+  document.querySelector(".products").style.display = "none";
+  document.querySelector(".location").style.display = "none";
+  document.querySelector(".news").style.display = "none";
+  document.querySelector(".guest").style.display = "none";
+  document.querySelector(".signup").style.display = "none";
+  document.querySelector(".signin").style.display = "none";
 }
 
 function showProductsTab() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("products").style.display = "block";
-  document.getElementById("itemsTable").style.display = "block";
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".products").style.display = "block";
+  document.querySelector(".itemsTable").style.display = "block";
   getItems("itemTable");
-  document.getElementById("location").style.display = "none";
-  document.getElementById("news").style.display = "none";
-  document.getElementById("guest").style.display = "none";
-  document.getElementById("register").style.display = "none";
-  document.getElementById("signin").style.display = "none";
+  document.querySelector(".location").style.display = "none";
+  document.querySelector(".news").style.display = "none";
+  document.querySelector(".guest").style.display = "none";
+  document.querySelector(".signup").style.display = "none";
+  document.querySelector(".signin").style.display = "none";
 }
 
 function showLocationTab() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("products").style.display = "none";
-  document.getElementById("location").style.display = "block";
-  document.getElementById("news").style.display = "none";
-  document.getElementById("guest").style.display = "none";
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".products").style.display = "none";
+  document.querySelector(".location").style.display = "block";
+  document.querySelector(".news").style.display = "none";
+  document.querySelector(".guest").style.display = "none";
+  document.querySelector(".signup").style.display = "none";
   getLocations();
-  document.getElementById("register").style.display = "none";
-  document.getElementById("signin").style.display = "none";
+  document.querySelector(".signin").style.display = "none";
 }
 
 function showNewsTab() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("products").style.display = "none";
-  document.getElementById("location").style.display = "none";
-  document.getElementById("news").style.display = "block";
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".products").style.display = "none";
+  document.querySelector(".location").style.display = "none";
+  document.querySelector(".news").style.display = "block";
   getNews("newsTable");
-  document.getElementById("guest").style.display = "none";
-  document.getElementById("register").style.display = "none";
-  document.getElementById("signin").style.display = "none";
+  document.querySelector(".guest").style.display = "none";
+  document.querySelector(".signup").style.display = "none";
+document.querySelector(".signin").style.display = "none";
 }
 
 function showGuestTab() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("products").style.display = "none";
-  document.getElementById("location").style.display = "none";
-  document.getElementById("news").style.display = "none";
-  document.getElementById("guest").style.display = "block";
-  document.getElementById("register").style.display = "none";
-  document.getElementById("signin").style.display = "none";
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".products").style.display = "none";
+  document.querySelector(".location").style.display = "none";
+  document.querySelector(".news").style.display = "none";
+  document.querySelector(".guest").style.display = "block";
+  document.querySelector(".signup").style.display = "none";
+document.querySelector(".signin").style.display = "none";
 }
 
-function showRegisterTab() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("products").style.display = "none";
-  document.getElementById("location").style.display = "none";
-  document.getElementById("news").style.display = "none";
-  document.getElementById("guest").style.display = "none";
-  document.getElementById("register").style.display = "block";
-  document.getElementById("signin").style.display = "none";
+function showSignupTab() {
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".products").style.display = "none";
+  document.querySelector(".location").style.display = "none";
+  document.querySelector(".news").style.display = "none";
+  document.querySelector(".guest").style.display = "none";
+  document.querySelector(".signup").style.display = "block";
+  document.querySelector(".signin").style.display = "none";
 }
 
-function showSignInTab() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("products").style.display = "none";
-  document.getElementById("location").style.display = "none";
-  document.getElementById("news").style.display = "none";
-  document.getElementById("guest").style.display = "none";
-  document.getElementById("register").style.display = "none";
-  document.getElementById("signin").style.display = "block";
+function showSigninTab() {
+  document.querySelector(".home").style.display = "none";
+  document.querySelector(".products").style.display = "none";
+  document.querySelector(".location").style.display = "none";
+  document.querySelector(".news").style.display = "none";
+  document.querySelector(".guest").style.display = "none";
+  document.querySelector(".signup").style.display = "none";
+  document.querySelector(".signin").style.display = "block";
 }
 
 function getItems() {
@@ -103,27 +97,36 @@ function getItems() {
 }
 
 function showItems(data) {
-  var json = data;
-  let items = "";
+  const json = data;
+  let itemLayout = "";
+  let count = 0;
+  const itemImg = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=";
 
-  for (var key in json) {
-    var url = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=" + json[key].ItemId;
-    var img = `<img class="itemImg" src="` + url + `">`;
-    var itemTitle = json[key].Title;
-    var itemPrice = json[key].Price;
-    var itemOrigin = json[key].Origin;
-    var id = json[key].ItemId;
+  const addItem = item => {
+    itemLayout +=
+      "<td>" +
+        "<img src=" + itemImg + item.ItemId + " height=125 width=125>" +
+      "</td>" +
+      
+      "<td>" +
+          "<b>" + item.Title + "</b>" + 
+          "<br>" + 
+          item.Origin + 
+          "<br><b class=itemPrice>$" + 
+          item.Price + 
+          "</b><br>" + 
+          item.Type +
+          "<br><button type=submit class=buy>Buy</button>" +
+      "</td>";
+    count += 1;
 
-    
-    items +=
-    img + 
-    "<h2>" + itemOrigin + "</h2>" + 
-    "<h2 id=`itemTitle`>" + itemTitle + "</h2>" +
-    "<h2>" + "<span>" + "$" + "</span>" + itemPrice + " </h2>" +
-    "<button onclick=buyItem(" + id + ")>" +  "Buy Item </button>" + 
-    "<hr>";
-  }
-  document.getElementById("itemsTable").innerHTML = items;
+    if (count >= 4) {
+      itemLayout += "</tr>\n"
+      count = 0;
+    }
+  };
+  json.forEach(addItem);
+  document.querySelector(".itemsTable").innerHTML = itemLayout;
 }
 
 function searchItem() {
@@ -161,9 +164,6 @@ function getLocations() {
   .then((data) => {
     showLocations(data);
   })
-  .catch((error) => {
-    console.error(error);
-  })
 }
 
 /**
@@ -178,27 +178,31 @@ function showLocations(data) {
   const dictionary = {};
   
   for (var i = 0; i < line.length; i++) {
-    var lineArray = line[i].split(":");
-    var lineHeaders = lineArray[0].split(";")[0];
-
+    const lineArray = line[i].split(":");
+    //console.log(lineArray);
+    const lineHeaders = lineArray[0].split(";")[0]; // text headers
+    //console.log(lineHeaders);  
     let value;
     if (lineHeaders == "ADR") {
-      value = lineArray[1].slice(2, lineArray[1].length); 
-      value = lineArray[1].split(";");
-      value = value.slice(2, value.length).join(",");
+      value = lineArray[1].slice(2, lineArray[1].length); //get address value from the array
+      value = lineArray[1].split(";"); //split array in semicolons separately
+      value = value.slice(2, value.length).join(","); //joins the address into one string
     }
     else {
       value = lineArray[1];
     }
     dictionary[lineHeaders] = value;
   }
-
-  document.getElementById("locationContent").innerHTML = dictionary["ADR"] + "<br>" + 
-  "<a href=mailto:" + dictionary["EMAIL"] + ">" + dictionary["EMAIL"] + "</a><br>" + 
-  "<a href=tel:" + dictionary["TEL"] + ">" + dictionary["TEL"] + "</a><br>" + 
-  "<a href=http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/vcard>Please add us to your contacts!</a>";
+  document.querySelector("#locationContent").innerHTML = 
+    "Address: " + dictionary["ADR"] + "<br>" + 
+    "Email: " + "<a class=vcard href=mailto:" + dictionary["EMAIL"] + ">" + dictionary["EMAIL"] + "</a><br>" + 
+    "Phone: " + "<a class=vcard href=tel:" + dictionary["TEL"] + ">" + dictionary["TEL"] + "</a><br>" +
+    "Download VCard: " + 
+    "<button type=button class=vcardBtn>" +
+      "<a href=http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/vcard>🖫</a>" +
+    "</button>";
+    
 }
-
 
 function getNews(items) {
   const newsEndpoint = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/news";
@@ -227,27 +231,31 @@ function getNews(items) {
  */
 
 function showNews(data) {
+  console.log(data);
   var json = data;
-  let items = "";
+  let newsLayout = "";
+  let count = 0;
+  const addNews = news => {
+    newsLayout +=
+      "<td>" +
+        "<img class=newsImg src=" + news.enclosureField.urlField + ">" +
+      "</td>" +
+      "<td>" +
+        "<a href=" + news.linkField + ">" +
+            "<b>" + news.titleField + "</b>" + 
+        "</a>" +
+        "<br>" +
+        "<p>" + news.descriptionField + "</p>" +
+        "<b>Date Published: " + news.pubDateField.substring(0, 16) + "</b>";
+    count += 1;
 
-  for (var key in json) {
-    var record = json[key];
-    var newsTitle = record.titleField;
-    var newsLinkURL = record.linkField;
-    var newsImgURL = record.enclosureField.urlField;
-    var newsPublishDate = record.pubDateField;
-    var newsDescription = record.descriptionField;
-
-    items +=
-    "<div class=`newsContent`>" + 
-    "<h2>" + `<a href="` + newsLinkURL + `">` + newsTitle + `</a>` + "</h2>" + 
-    "<div>" + `<img class="newsImg" src="` + newsImgURL + `" >` + "</div>" + 
-    "<div>" + "<h3>" + newsPublishDate + "</h3>" + "</div>" +
-    "<p>" + newsDescription + "</p>" + 
-    "<hr>" + 
-    "</div>";
+    if (count >= 1) {
+      newsLayout += "</tr>\n"
+      count = 0;
+    }
   }
-  document.getElementById("newsTable").innerHTML = items;
+  json.forEach(addNews);
+  document.querySelector("#newsTable").innerHTML = newsLayout;
 }
 
 /**
@@ -274,118 +282,11 @@ function postComment() {
     return response.json();
   })
   .then(() => {
-    document.getElementById("commentSection").src = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/htmlcomments";
+    document.querySelector(".commentSection").src = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/htmlcomments";
   })
 }
 
-/**
-*
-* Function handles registering a user
-*
- */
-function register() {
-  var email = document.getElementById("email").value;
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  var formData = {
-      "Email": email,
-      "Name": username,
-      "Password": password
-  };
-  if (email.length && username.length && password.length > 0) {
-  var registerEndpoint = "http://localhost:8188//DairyService.svc/register";
-  fetch(registerEndpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(formData),
-  })
-  .then((response) => {
-      if (response.status === 200) {
-        document.getElementById("registerMessage").innerHTML = "Registered!";
-        document.getElementById("email").value = "";
-        document.getElementById("username").value = "";
-        document.getElementById("password").value = "";
-      }
-      else {
-        alert("Registration not successful!");
-      }
-      return response.json();
-    })
-  } else {
-    alert("All fields must be filled in!");
-  }
-}
-
-/**
-*
-* Function handles buying an item when the user is logged in else it directs to sign in page 
-*
- */
-
-function buyItem(id) {
-  if (loginUsername == "" || loginPassword == "") {
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    var buyEndpoint = "http://localhost:8189/Service.svc/buy?id=" + id;
-    xhr.open("GET", buyEndpoint, true, loginUsername, loginPassword);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        var xml = xhr.responseText;
-        console.log(xml);
-      }
-    }
-  } else {
-    showSignInTab();
-  }
-}
-
-/**
-*
-* Function displays message when user is signed in or signed out
-*
- */
-
-function showSignIn() {
-  if (!signedIn) {
-    showSignInTab();
-  }
-  else {
-    signedIn = false;
-    document.getElementById("signinMessage").innerHTML = "Signed in!";
-    loginUsername = "";
-    loginPassword = "";
-    document.getElementById("signinMessage").innerHTML = "Signed out!";
-  }
-}
-
-/**
-*
-* Function handling a registered user sign in 
-*
-**/
-
-function signIn() {
-  loginUsername = document.getElementById("loginUsername").value;
-  loginPassword = document.getElementById("loginPassword").value;
-
-  if (loginUsername.length && loginPassword.length > 0) {
-    let xhr = new XMLHttpRequest();
-    let userEndpoint = "http://localhost:8189/Service.svc/user"; 
-		xhr.open("GET", userEndpoint, true, loginUsername, loginPassword);
-    xhr.withCredentials = true;
-    xhr.onload = () => {
-			if (this.status === 200) {
-        showProductsTab();
-        signedIn = true;
-				document.getElementById("signinMessage").innerHTML = "Signed in successful!";
-			} else {
-				document.getElementById("signinMessage").innerHTML = "Signed in unsuccessful!"; 
-			}      
-		}
-		xhr.send();
-	} else {
-		alert("Please enter your Username and Password");
-	}
-}
+const guestForm = document.querySelector(".guest-form");
+guestForm.addEventListener("submit", e => {
+  e.preventDefault();
+})
